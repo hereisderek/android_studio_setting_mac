a952703d1d13f0f932def5b6beed614409629162
Derek Zhu <derek@roamltd.com>
2015-09-19 00:01:15 +1200

    java class templates add final key word to tag

--- fileTemplates/internal/Class.java
+++ fileTemplates/internal/Class.java
@@ -1,5 +1,5 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
 public class ${NAME} {
-    private static String TAG = ${NAME}.class.getSimpleName();
+    private static final String TAG = ${NAME}.class.getSimpleName();
 }
