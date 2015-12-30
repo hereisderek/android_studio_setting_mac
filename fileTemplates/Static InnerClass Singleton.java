42680b83ccf251230a0f0a0b75d1924cfafb838b
Derek Zhu <derek@roamltd.com>
2015-12-30 23:10:47 +1300

    TAG instead "private static final String TAG = ${NAME}.class.getSimpleName();"  now using "private static final String TAG = "${NAME}";"

--- fileTemplates/Static InnerClass Singleton.java
+++ fileTemplates/Static InnerClass Singleton.java
@@ -1,7 +1,7 @@
 #if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
 #parse("File Header.java")
 public class ${NAME}{
-    private static final String TAG = ${NAME}.class.getSimpleName();
+    #parse("Java TAG.java")
     
     private static class SingletonHolder {
         private static final ${NAME} INSTANCE = new ${NAME}();  
